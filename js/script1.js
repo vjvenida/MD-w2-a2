94795070d7dba9b688b14393b0507e4a6a026022
vjvenida <vjvenida@gmail.com>
2017-05-13 15:12:38 +0800

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,4 @@
 document.querySelector("#appdemo2").innerHTML = "<button class='btn btn-primary' id='showname'>Click Me</button>";
 $('#showname').click(function(){
-	alert("I am Clyde");
+	alert("I am Batman");
 });
